057952564f64ad25b01ade4d5e74eafb31c0cb53
Clucch <mattiacerutti04@gmail.com>
2023-08-27 17:02:48 +0200

    Updated notion script

--- notion_importer.py
+++ notion_importer.py
@@ -4,28 +4,85 @@ import json
 
 # Notion API credentials
 NOTION_API_KEY = ''
-DATABASE_ID = ''
+SCHEDULE_DATABASE_ID = ''
+CLASSES_DATABASE_ID = ''
 
 # Endpoint URLs
 NOTION_URL = 'https://api.notion.com/v1/pages'
 
-# Function to add an entry to the Notion database
-def addClassToNotion(className, startDate, startTime, finishDate, finishTime):
+def makeRequest(url= NOTION_URL, data=None):
+
     headers = {
-        'Authorization': f'Bearer {NOTION_API_KEY}',
-        'Content-Type': 'application/json',
-        'Notion-Version': '2022-06-28' 
+        "Authorization": f"Bearer {NOTION_API_KEY}",
+        "Content-Type": "application/json",
+        "Notion-Version": "2021-05-13"
     }
 
-    startDatetime = f'{startDate}T{startTime}:00Z'
-    finishDatetime = f'{finishDate}T{finishTime}:00Z'
+
+    response = requests.post(url, headers=headers, json=data)
+
+    return response
+
+
+def checkIfClassExists(className):
+
+    response = makeRequest(url=f"https://api.notion.com/v1/databases/{CLASSES_DATABASE_ID}/query")
+    response_data = response.json()
+
+    for entry in response_data.get("results", []):
+        properties = entry.get("properties", {})
+        name_property = properties.get("CLASS_FULL_NAME", {}).get("rich_text", [])[0].get("plain_text", "")
+        if name_property.lower() == className.lower():
+            return entry["id"]
+        
+    #If not found, create one
+    data = {
+        "parent": {
+            "database_id": CLASSES_DATABASE_ID
+        },
+        "properties": {
+            "Name": {
+                "title": [
+                    {
+                        "text": {
+                            "content": className
+                        }
+                    }
+                ]
+            },
+            "CLASS_FULL_NAME": {
+                "rich_text": [
+                {
+                    "type": "text",
+                    "text": {
+                        "content": className,
+                    }
+                }
+            ]
+            }
+        }
+    }
+
+    response = makeRequest(data=data)
+    response_json = response.json()
+    if(response.status_code != 200):
+        raise Exception(f"An error occurred while adding the class {className} to Notion. Here is the error in detail: {response_json['message']}")
+    else:
+        print(f"Class {className} successfully added to Notion!")
+        return response_json["id"]
+
+# Function to add an entry to the Notion database
+def addClassToNotion(className, classId, startDate, startTime, finishDate, finishTime):
+
+    startDatetime = f'{startDate}T{startTime}+02:00'
+    finishDatetime = f'{finishDate}T{finishTime}+02:00'
 
     data = {
         "parent": {
-            "database_id": DATABASE_ID
+            "database_id": SCHEDULE_DATABASE_ID
         },
         "properties": {
-            "Class": {
+            "Name": {
                 "title": [
                     {
                         "text": {
@@ -38,23 +95,29 @@ def addClassToNotion(className, startDate, startTime, finishDate, finishTime):
                 "type": "date",
                 "date": {
                     "start": startDatetime,
-                    "end": finishDatetime
+                    "end": finishDatetime,
                 }
+            },
+            "Class": {
+                "relation": [{"id": classId}]
             }
         }
 }
 
-    response = requests.post(NOTION_URL, headers=headers, json=data)
+   
+    response =  makeRequest(data=data)
     response_json = response.json()
     if(response.status_code != 200):
-        raise Exception(f"An error occurred while adding the class {className} to Notion. Here is the error in detail: {response_json['message']}")
+        raise Exception(f"An error occurred while adding the schedule of class {className} to Notion. Here is the error in detail: {response_json['message']}")
 
     else:
-        print(f"Class {className} successfully added to Notion!")
+        print(f"Schedule of class {className} successfully added to Notion!")
 
 if __name__ == '__main__':
     
     jsonFilePath = input("Type the full path of the json file you want to import: ")
+    if jsonFilePath == "":
+        jsonFilePath = "schedule.json"
 
     scheduleDictionary = {}
     with open(jsonFilePath) as file:
@@ -71,7 +134,9 @@ if __name__ == '__main__':
                     start_time = hour.split('-')[0].strip()
                     finish_time = hour.split('-')[1].strip()
 
-                    addClassToNotion(className, date, start_time, date, finish_time)
+                    classId = checkIfClassExists(className)
+
+                    addClassToNotion(className, classId, date, start_time, date, finish_time)
         
         print("Schedule successfully added to Notion!")
 
